6bfdff28c8e531a024072cf6a483ffc9a7b540d2
xxwygj3 <xxwygj3@126.com>
2019-06-11 20:57:12 +0800

    Initial commit

---
+++ src/main/java/com/testjava/mybatisGen/dao/UserinfoMapper.java
@@ -0,0 +1,30 @@
+package com.testjava.mybatisGen.dao;
+
+import com.testjava.mybatisGen.entiry.Userinfo;
+import com.testjava.mybatisGen.entiry.UserinfoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserinfoMapper {
+    int countByExample(UserinfoExample example);
+
+    int deleteByExample(UserinfoExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Userinfo record);
+
+    int insertSelective(Userinfo record);
+
+    List<Userinfo> selectByExample(UserinfoExample example);
+
+    Userinfo selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Userinfo record, @Param("example") UserinfoExample example);
+
+    int updateByExample(@Param("record") Userinfo record, @Param("example") UserinfoExample example);
+
+    int updateByPrimaryKeySelective(Userinfo record);
+
+    int updateByPrimaryKey(Userinfo record);
+}
---
+++ src/main/java/com/testjava/mybatisGen/entiry/Userinfo.java
@@ -0,0 +1,55 @@
+package com.testjava.mybatisGen.entiry;
+
+import java.util.Date;
+
+public class Userinfo {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Integer age;
+
+    private Date insertDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getInsertDate() {
+        return insertDate;
+    }
+
+    public void setInsertDate(Date insertDate) {
+        this.insertDate = insertDate;
+    }
+}
